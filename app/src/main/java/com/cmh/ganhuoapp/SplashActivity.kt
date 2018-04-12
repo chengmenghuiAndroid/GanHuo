8d839a90028516ee10dc31a3e105172fe51b8678
PC-20171014LCHO\Administrator <cheng_oop@foxmail.com>
2018-04-12 13:27:22 +0800

    提交代码

---
+++ app/src/main/java/com/cmh/ganhuoapp/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.cmh.ganhuoapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+class SplashActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
